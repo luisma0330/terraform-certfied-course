2eb0985bc3b76b025943a02ffd1680bd8ce10387
Luis Ojeda <luis.manuel.ojeda@accenture.com>
2024-01-03 18:16:31 -0600

    Adding a s3 bucket with random id - Terraform Resourse Block

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 #Retrieve the list of AZs in the current AWS region
@@ -152,10 +152,49 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
 
   subnet_id              = aws_subnet.public_subnets["public_subnet_1"].id
-  vpc_security_group_ids = ["sg-0ecdd14343e6fa8b3"]
+  vpc_security_group_ids = [aws_security_group.my-new-security-group.id]
 
   tags = {
     "terraform" = "true"
     Name = "Web Server"
   }
+}
+
+resource "aws_s3_bucket" "my-new-S3-bucket" {
+  bucket = "my-new-tf-test-bucket-${random_id.randomness.hex}"
+
+  tags = {
+    Name = "My S3 bucket"
+    Purpuse = "Intro to Resource Block Lab"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "my_new_bucket_acl" {   
+  bucket = aws_s3_bucket.my-new-S3-bucket.id  
+  rule {     
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_security_group" "my-new-security-group" {
+  name        = "web_server_inbound"
+  description = "Allow inbound traffic on tcp/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow 443 from the Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "web_server_inbound"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
+}
+
+resource "random_id" "randomness" {
+  byte_length = 16
 }
